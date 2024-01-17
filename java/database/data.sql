240c99da30003cbbf17b8ad65d35cfe027e16899
Jack Telford <Wipke9893@yahoo.com>
2024-01-17 15:00:32 -0500

    new

--- java/database/data.sql
+++ java/database/data.sql
@@ -8,3 +8,31 @@ INSERT INTO users (username,password_hash,role) VALUES ('jack','$2a$10$xVN8wyDaN
 
 
 
+BEGIN TRANSACTION;
+
+-- Inserting into species table
+INSERT INTO species (common_name, scientific_name, color, bloom_time, size_growth_habit)
+VALUES ('Oak', 'Quercus', 'Green', 'Spring', 'Large tree, up to 60 feet');
+
+INSERT INTO species (common_name, scientific_name, color, bloom_time, size_growth_habit)
+VALUES ('Maple', 'Acer', 'Red', 'Autumn', 'Medium tree, up to 40 feet');
+
+INSERT INTO species (common_name, scientific_name, color, bloom_time, size_growth_habit)
+VALUES ('Pine', 'Pinus', 'Green', 'Non-flowering', 'Tall tree, up to 80 feet');
+
+INSERT INTO species (common_name, scientific_name, color, bloom_time, size_growth_habit)
+VALUES ('Birch', 'Betula', 'Green', 'Spring', 'Medium tree, up to 50 feet');
+
+INSERT INTO species (common_name, scientific_name, color, bloom_time, size_growth_habit)
+VALUES ('Cherry', 'Prunus', 'Pink', 'Spring', 'Small tree, up to 30 feet');
+
+-- Assuming species_id generated are 1, 2, 3, 4, 5
+-- Replace the values for other columns as per your requirement
+
+-- Inserting into trees table
+INSERT INTO trees (species_id, common_name, scientific_name, color, bloom_time, size_growth_habit, hardiness_zone, light_requirement, watering_needs, soil, leaf, special, lifespan, maintenance, uses, pest_disease, origin, image)
+VALUES (1, 'Oak', 'Quercus', 'Green', 'Spring', 'Large tree, up to 60 feet', '3-9', 'Full sun', 'Medium', 'Well-drained', 'Deciduous', 'Acorns as fruit', '100 years', 'Low', 'Shade tree', 'Oak wilt, root rot', 'Northern Hemisphere', 'http://example.com/oak.jpg');
+
+-- Add more trees similarly for species_id 2, 3, 4, 5
+
+COMMIT;
